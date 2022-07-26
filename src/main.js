a10a600fc86e7b41afbf340b1b62c318b858b386
JCCiasico <carlociasico01@gmail.com>
2022-07-26 09:07:27 +0800

    fix: fixed dependencies

--- src/main.js
+++ src/main.js
@@ -2,23 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import{BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-import * as VueGoogleMaps from "vue2-google-maps"
-import FullpageModal from 'vue-fullpage-modal'
-import PopupLayer from 'vue-popup-layer'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(FullpageModal)
 Vue.use(router)
-Vue.use(PopupLayer)
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyDyD80C2Q15twhFBRnp01eFjYghbw8FRhk",
-    libraries: "places"
-  }
-});
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
