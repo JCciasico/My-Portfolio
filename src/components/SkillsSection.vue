e38d36650079a603c07741fc2b26183b40ffb347
JCCiasico <carlociasico01@gmail.com>
2022-07-26 14:04:20 +0800

    chore: added skills in skills-section

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -6,12 +6,29 @@
             <h1 class="text-2">Skills</h1>
         </div>
         <div class="row border">
-            <div class="col-lg-6">
-                <div class="row">
-                </div>
+            <div class="col-lg-6 col-md-12 p-3">
+                    <div class="p-3 border text-center text-white box-1">
+                        <span class="skill-item">Vue js</span>
+                        <i class="fa-brands fa-vuejs icon"></i>
+                    </div>
+                    <div class="p-3 border text-center text-white">
+                        <span>HTML</span>
+                        <i class="fa-brands fa-html5 icon"></i>
+                    </div>
+                    <div class="p-3 border text-center text-white">
+                        <span>CSS</span>
+                        <i class="fa-brands fa-css3-alt icon"></i>
+                    </div>
+                    <div class="p-3 border text-center text-white">
+                        <span>Javascript</span>
+                        <i class="fa-brands fa-js icon"></i>
+                    </div>
             </div>
-            <div class="col-lg-6">
-
+            <div class="col-lg-6 col-md-12 p-3">
+                    <div class="border">PHP</div>
+                    <div class="border">Bootstrap</div>
+                    <div class="border">Axios</div>
+                    <div class="border">MySQL</div>
             </div>
         </div>
         <div class="container text-center footer-details">
@@ -47,5 +64,18 @@ h3{
 }
 .footer-details{
     padding: 1rem;
+}
+.box-1{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    column-gap: 10px;
+}
+.skill-item{
+    font-size: 2rem;
+    font-weight: bold;
+}
+.icon{
+    font-size: 3rem;
 }
 </style>
