e2de3bb305bb12e6245d2980ba84466077bdb4dd
JCCiasico <carlociasico01@gmail.com>
2022-07-27 13:07:52 +0800

    chore: fixed services section

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,39 +1,36 @@
 <template>
     <section>
-        <div class="container">
+        <div class="container" style="padding-bottom: 2.5rem;">
             <div class="container-fluid text-center" style="padding-top:2rem">
                 <h1 class="text-1">Services</h1>
                 <h1 class="text-2">Services</h1>
             </div>
-            <div class="row" style="padding-bottom: 2rem;">
-                <div class="col-lg-4 col-md-12 border">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="../assets/carlo.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
+                <div class="row services-body" data-aos="fade">
+                    <div class="col-lg-2 col-md-6 col-sm-12 box-1">
+                        <div class="text-center">
+                            <i class="fa-solid fa-laptop-code icon"></i><br>
+                            <span>Web Develop/Design</span>
+                        </div>
+                    </div>
+                   <div class="col-lg-2 col-md-6 col-sm-12 box-1">
+                        <div class="text-center">
+                            <i class="fa-solid fa-code-merge icon"></i><br>
+                            <span>API Integration</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-12 box-1">
+                        <div class=" ext-center">
+                            <i class="fa-solid fa-list-check icon"></i><br>
+                            <span>Quality Testing</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-12 box-1">
+                        <div class="text-center">
+                            <i class="fa-solid fa-mobile-screen icon"></i><br>
+                            <span>Responsive Design</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-12 border">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="../assets/carlo.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12 border">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="../assets/carlo.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </section>
 </template>
@@ -58,5 +55,42 @@ export default {
     margin-bottom: 75px;
     font-size: 3rem;
 }
+.icon{
+    color: #ff7f50;
+    font-size: 5rem;
+    padding: 1rem;
+}
+.box-1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    padding: 2rem;
+}
+.services-body{
+    display: flex;
+    justify-content: center;
+}
+@media(max-width:900px){
+    .text-1{
+        font-size: 4.5rem;
+    }
+    .text-2{
+        font-size: 2rem;
+        margin-top: -65px;
+
+    }
+}
+@media(max-width:500px){
+    .text-1{
+        font-size: 4rem;
+    }
+    .text-2{
+        font-size: 2rem;
+        margin-top: -60px;
+
+    }
+}
+
 
 </style>
